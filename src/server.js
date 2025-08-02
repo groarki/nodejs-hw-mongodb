65ff5135cac34df3a3ccf2a66b69ab7f2d0791d5
Yelyzaveta Shevchenko <lizkasheva26@gmail.com>
2025-08-02 19:53:12 +0200

    add json status

--- src/server.js
+++ src/server.js
@@ -25,6 +25,7 @@ export const setupServer = () => {
     const contacts = await getAllContacts();
 
     res.status(200).json({
+      status: 200,
       message: 'Successfully found contacts!',
       data: contacts,
     });
@@ -43,6 +44,7 @@ export const setupServer = () => {
     }
 
     res.status(200).json({
+      status: 200,
       message: `Successfully found contact with id ${contactId}!`,
       data: contact,
     });
@@ -51,6 +53,7 @@ export const setupServer = () => {
   // 404
   app.use((req, res, next) => {
     res.status(404).json({
+      status: 404,
       message: 'Not found',
     });
   });
